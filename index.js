1c827b0ec17221a66f3115a6ce0efb6878ede1f3
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-27 22:12:13 -0400

    expose constructors, use `isType` on configs

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ var clone = require('clone-deep');
 var Base = require('app-base');
 var Node = require('./lib/node');
 var Target = require('./lib/target');
+var Files = require('./lib/files');
 var Task = require('./lib/task');
 var utils = require('./lib/utils');
 
 function Config(name, config) {
   Base.call(this);
+  if (typeof name !== 'string') {
+    config = name;
+    name = 'root';
+  }
   this.name = name;
   this.define('orig', clone(config || {}));
+  this.options = {};
   this.config = {};
   this.tasks = {};
   this.targets = {};
@@ -20,22 +26,31 @@ function Config(name, config) {
 Base.extend(Config);
 
 Config.prototype.create = function(key, config) {
-  if (utils.isTask(config, key)) {
-    this.tasks[key] = new Task(key, config, this);
-  } else if (utils.isTarget(config, key)) {
-    this.targets[key] = new Target(key, config, this);
-  } else if (utils.isNode(config, key)) {
-    this.config[key] = new Node(config, this);
-  } else {
-    this.config[key] = config;
+  var type = utils.isType(config, key);
+
+  switch(type) {
+    case 'options':
+      if (key === 'options') {
+        utils.merge(this.options, config);
+      } else {
+        this.options[key] = config;
+      }
+      break;
+    case 'task':
+      this.tasks[key] = new Task(key, config, this);
+      break;
+    case 'target':
+      this.targets[key] = new Target(key, config, this);
+      break;
+    case 'node':
+      this.config[key] = new Node(config, this);
+      break;
+    default: {
+      this.config[key] = config;
+      break;
+    }
   }
 };
-
-  // validate: function (name) {
-  //   if (~reserved.methods.indexOf(name)) {
-  //     throw new Error(name + ' is a reserved root-level property.');
-  //   }
-  // }
 
 /**
  * Expose `Config`
@@ -44,13 +59,10 @@ Config.prototype.create = function(key, config) {
 module.exports = Config;
 
 /**
- * Expose `Task`
+ * Expose constructors
  */
 
 module.exports.Task = Task;
-
-/**
- * Expose `Target`
- */
-
 module.exports.Target = Target;
+module.exports.Files = Files;
+module.exports.Node = Node;
